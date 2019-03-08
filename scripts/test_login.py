3cc6786596a525c044ccd677356133df767d9c25
ForrestCo <144702575@qq.com>
2019-03-08 11:03:56 +0800

    第一次提交

---
+++ scripts/test_login.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# !/usr/bin/env python
+# @File  : test_login.py
+# @Author: 杨崇
+# @Date  : 2019/3/8
+# @Desc  : null
+
+
+class TestLogin:
+    def test_login1(self):
+        print("----------test_login1------执行")
+        assert 0
+
+    def test_login2(self):
+        print("----------test_login2------执行")
+        assert 1
+
+    def test_login3(self):
+        print("----------test_login3------执行")
+        assert 0
